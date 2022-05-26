a9ce0e9da884ed4e4b71c58054cce3c8424179b4
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-05-26 18:23:53 -0500

    Add button

---
+++ modules/sample-liferay-fragments/src/sample-collection/sample-fragment-with-react/Button.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function (props) {
+  return <button className="btn btn-primary">{props.text}</button>;
+}
--- modules/sample-liferay-fragments/src/sample-collection/sample-fragment-with-react/main.js
+++ modules/sample-liferay-fragments/src/sample-collection/sample-fragment-with-react/main.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
+import Button from "./Button";
 
 export default function (props) {
   console.log("props", props);
 
-  return <h1>Hello world</h1>;
+  return (
+    <div>
+      <Button text="Hi There"></Button>
+      <h1>Hello world IS again!</h1>
+    </div>
+  );
 }
